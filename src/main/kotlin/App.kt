d1e04c5b8671238b5b287641ed1b551f16743533
mao <mao.lembryo@gmail.com>
2019-11-03 05:42:08 +0900

    サンプルをもうちょい修正(・∀・)。

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,8 +1,8 @@
 import com.lembryo.ougi.http.WebServer
 import com.lembryo.ougi.http.default.ParamsMiddleware
-import com.lembryo.ougi.http.default.Renderer
 import com.lembryo.ougi.http.default.ResponseMiddleware
 import com.lembryo.ougi.http.default.RoutingMiddleware
+import com.lembryo.ougi.http.html.Renderer
 
 fun main(argv: Array<String>) {
 
@@ -12,7 +12,17 @@ fun main(argv: Array<String>) {
     app.use(ResponseMiddleware()) // レスポンスに Date や Content-Length などを付加
     app.use(RoutingMiddleware {
         get {
-            Renderer.html("index.kts")
+            // HTMLレンダラに渡すデータを作成
+            val bindings = mutableMapOf<String, Any>()
+            bindings["title"] = "リスト"
+            bindings["data"] = mapOf(
+                "2019/11/02" to "昨日",
+                "2019/11/03" to "今日",
+                "2019/11/04" to "明日",
+                "2019/11/05" to "明後日"
+            )
+            // レンダリング
+            Renderer.html("index.kts", bindings)
         }
     })
 
